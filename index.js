4f00bd33984b6b5e9a4e78b7ea89484620e8eee7
syacyshyn <shaynayacyshyn@gmail.com>
2023-09-26 10:11:40 -0700

    - added js logic for diagonal wins - incremented scoreboard and gave functionality to refresh button

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const arrayOfIndividualSquareElements = document.getElementsByClassName("individ
 const mapOfIdsAndElements = htmlCollectionOfElementsTurnedIntoMap(arrayOfIndividualSquareElements);
 const turnButton = document.getElementById("turn-button");
 let roundCounter = 1;
+const winnerScreen = document.getElementById("winner-screen");
 let playersTurn = "X";
+let winner;
 turnButton.innerText = `${playersTurn} TURN`;
+let xScoreElement = document.getElementById("x-score");
+let oScoreElement = document.getElementById("o-score");
+let tieScoreElement = document.getElementById("tie-score");
+let refreshButton = document.getElementById("refresh-button");
+let xScore = 0;
+let oScore = 0;
+let tiesScore = 0;
 
 function htmlCollectionOfElementsTurnedIntoMap(arrayOfHtmlElements) {
     const mapOfIdsAndElements = new Map();
@@ -30,13 +39,14 @@ function pickRandomCornerSquare() {
         mapOfIdsAndElements.get("bottom-left-square")];
     let randomNumber = Math.floor(Math.random() * (availableCornerSquares.length));
     availableCornerSquares[randomNumber].innerText = "O";
+    availableCornerSquares[randomNumber].disabled = true;
 }
 
 function cornersAreEmpty() {
     return (mapOfIdsAndElements.get("top-right-square").innerText === "" &&
         mapOfIdsAndElements.get("top-left-square").innerText === "" &&
         mapOfIdsAndElements.get("bottom-right-square").innerText === "" &&
-        mapOfIdsAndElements.get("bottom-right-square").innerText === "");
+        mapOfIdsAndElements.get("bottom-left-square").innerText === "");
 }
 
 function rowOfTwo() {
@@ -53,7 +63,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("top-left-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("top-left-square").innerText ===
             mapOfIdsAndElements.get("top-right-square").innerText) &&
         mapOfIdsAndElements.get("top-left-square").innerText !== "" &&
         mapOfIdsAndElements.get("top-middle-square").innerText === "") {
@@ -63,7 +74,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("top-middle-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("top-middle-square").innerText ===
             mapOfIdsAndElements.get("top-right-square").innerText) &&
         mapOfIdsAndElements.get("top-middle-square").innerText !== "" &&
         mapOfIdsAndElements.get("top-left-square").innerText === "") {
@@ -73,7 +85,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("middle-left-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("middle-left-square").innerText ===
             mapOfIdsAndElements.get("middle-middle-square").innerText) &&
         mapOfIdsAndElements.get("middle-middle-square").innerText !== "" &&
         mapOfIdsAndElements.get("middle-right-square").innerText === "") {
@@ -83,7 +96,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("middle-left-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("middle-left-square").innerText ===
             mapOfIdsAndElements.get("middle-right-square").innerText) &&
         mapOfIdsAndElements.get("middle-left-square").innerText !== "" &&
         mapOfIdsAndElements.get("middle-middle-square").innerText === "") {
@@ -93,7 +107,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("middle-middle-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("middle-middle-square").innerText ===
             mapOfIdsAndElements.get("middle-right-square").innerText) &&
         mapOfIdsAndElements.get("middle-middle-square").innerText !== "" &&
         mapOfIdsAndElements.get("middle-left-square").innerText === "") {
@@ -103,7 +118,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("bottom-left-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("bottom-left-square").innerText ===
             mapOfIdsAndElements.get("bottom-middle-square").innerText) &&
         mapOfIdsAndElements.get("bottom-middle-square").innerText !== "" &&
         mapOfIdsAndElements.get("bottom-right-square").innerText === "") {
@@ -113,7 +129,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("bottom-left-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("bottom-left-square").innerText ===
             mapOfIdsAndElements.get("bottom-right-square").innerText) &&
         mapOfIdsAndElements.get("bottom-left-square").innerText !== "" &&
         mapOfIdsAndElements.get("bottom-middle-square").innerText === "") {
@@ -123,7 +140,8 @@ function rowOfTwo() {
         if (symbol === "O") {
             return [isTrue, elementToBlockOrWin, symbol];
         }
-    } else if ((mapOfIdsAndElements.get("bottom-middle-square").innerText ===
+    }
+    if ((mapOfIdsAndElements.get("bottom-middle-square").innerText ===
             mapOfIdsAndElements.get("bottom-right-square").innerText) &&
         mapOfIdsAndElements.get("bottom-middle-square").innerText !== "" &&
         mapOfIdsAndElements.get("bottom-left-square").innerText === "") {
@@ -235,6 +253,138 @@ function columnOfTwo() {
     return [isTrue, elementToBlockOrWin, symbol];
 }
 
+function diagonalOfTwo() {
+    let isTrue = false;
+    let elementToBlockOrWin = null;
+    let symbol = null;
+    if ((mapOfIdsAndElements.get("top-left-square").innerText ===
+            mapOfIdsAndElements.get("middle-middle-square").innerText) &&
+        mapOfIdsAndElements.get("top-left-square").innerText !== "" &&
+        mapOfIdsAndElements.get("bottom-right-square").innerText === "") {
+        isTrue = true;
+        elementToBlockOrWin = mapOfIdsAndElements.get("bottom-right-square");
+        symbol = mapOfIdsAndElements.get("top-left-square").innerText;
+        if (symbol === "O") {
+            return [isTrue, elementToBlockOrWin, symbol];
+        }
+    } else if ((mapOfIdsAndElements.get("top-left-square").innerText ===
+            mapOfIdsAndElements.get("bottom-right-square").innerText) &&
+        mapOfIdsAndElements.get("top-left-square").innerText !== "" &&
+        mapOfIdsAndElements.get("middle-middle-square").innerText === "") {
+        isTrue = true;
+        elementToBlockOrWin = mapOfIdsAndElements.get("middle-middle-square");
+        symbol = mapOfIdsAndElements.get("top-left-square").innerText;
+        if (symbol === "O") {
+            return [isTrue, elementToBlockOrWin, symbol];
+        }
+    } else if ((mapOfIdsAndElements.get("middle-middle-square").innerText ===
+            mapOfIdsAndElements.get("bottom-right-square").innerText) &&
+        mapOfIdsAndElements.get("middle-middle-square").innerText !== "" &&
+        mapOfIdsAndElements.get("top-left-square").innerText === "") {
+        isTrue = true;
+        elementToBlockOrWin = mapOfIdsAndElements.get("top-left-square");
+        symbol = mapOfIdsAndElements.get("bottom-right-square").innerText;
+        if (symbol === "O") {
+            return [isTrue, elementToBlockOrWin, symbol];
+        }
+    } else if ((mapOfIdsAndElements.get("top-right-square").innerText ===
+            mapOfIdsAndElements.get("middle-middle-square").innerText) &&
+        mapOfIdsAndElements.get("top-right-square").innerText !== "" &&
+        mapOfIdsAndElements.get("bottom-left-square").innerText === "") {
+        isTrue = true;
+        elementToBlockOrWin = mapOfIdsAndElements.get("bottom-left-square");
+        symbol = mapOfIdsAndElements.get("top-right-square").innerText;
+        if (symbol === "O") {
+            return [isTrue, elementToBlockOrWin, symbol];
+        }
+    } else if ((mapOfIdsAndElements.get("top-right-square").innerText ===
+            mapOfIdsAndElements.get("bottom-left-square").innerText) &&
+        mapOfIdsAndElements.get("top-right-square").innerText !== "" &&
+        mapOfIdsAndElements.get("middle-middle-square").innerText === "") {
+        isTrue = true;
+        elementToBlockOrWin = mapOfIdsAndElements.get("middle-middle-square");
+        symbol = mapOfIdsAndElements.get("top-right-square").innerText;
+        if (symbol === "O") {
+            return [isTrue, elementToBlockOrWin, symbol];
+        }
+    } else if ((mapOfIdsAndElements.get("middle-middle-square").innerText ===
+            mapOfIdsAndElements.get("bottom-left-square").innerText) &&
+        mapOfIdsAndElements.get("middle-middle-square").innerText !== "" &&
+        mapOfIdsAndElements.get("top-right-square").innerText === "") {
+        isTrue = true;
+        elementToBlockOrWin = mapOfIdsAndElements.get("top-right-square");
+        symbol = mapOfIdsAndElements.get("middle-middle-square").innerText;
+        if (symbol === "O") {
+            return [isTrue, elementToBlockOrWin, symbol];
+        }
+    }
+    return [isTrue, elementToBlockOrWin, symbol];
+}
+
+function checkForWinner() {
+    if (mapOfIdsAndElements.get("top-left-square").innerText ===
+        mapOfIdsAndElements.get("top-middle-square").innerText &&
+        mapOfIdsAndElements.get("top-left-square").innerText === mapOfIdsAndElements.get("top-right-square").innerText &&
+        mapOfIdsAndElements.get
+        ("top-right-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("top-left-square").innerText;
+    } else if (mapOfIdsAndElements.get("middle-left-square").innerText ===
+        mapOfIdsAndElements.get("middle-middle-square").innerText &&
+        mapOfIdsAndElements.get("middle-left-square").innerText === mapOfIdsAndElements.get("middle-right-square").innerText &&
+        mapOfIdsAndElements.get
+        ("middle-right-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("middle-middle-square").innerText;
+    } else if (mapOfIdsAndElements.get("bottom-left-square").innerText ===
+        mapOfIdsAndElements.get("bottom-middle-square").innerText &&
+        mapOfIdsAndElements.get("bottom-left-square").innerText === mapOfIdsAndElements.get("bottom-right-square").innerText &&
+        mapOfIdsAndElements.get
+        ("bottom-right-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("bottom-left-square").innerText;
+    } else if (mapOfIdsAndElements.get("top-left-square").innerText ===
+        mapOfIdsAndElements.get("middle-left-square").innerText &&
+        mapOfIdsAndElements.get("top-left-square").innerText === mapOfIdsAndElements.get("bottom-left-square").innerText &&
+        mapOfIdsAndElements.get
+        ("top-left-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("top-left-square").innerText;
+    } else if (mapOfIdsAndElements.get("top-middle-square").innerText ===
+        mapOfIdsAndElements.get("middle-middle-square").innerText &&
+        mapOfIdsAndElements.get("top-middle-square").innerText === mapOfIdsAndElements.get("bottom-middle-square").innerText &&
+        mapOfIdsAndElements.get
+        ("bottom-middle-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("bottom-middle-square").innerText;
+    } else if (mapOfIdsAndElements.get("top-right-square").innerText ===
+        mapOfIdsAndElements.get("middle-right-square").innerText &&
+        mapOfIdsAndElements.get("top-right-square").innerText === mapOfIdsAndElements.get("bottom-right-square").innerText &&
+        mapOfIdsAndElements.get
+        ("top-right-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("top-right-square").innerText;
+    } else if (mapOfIdsAndElements.get("top-left-square").innerText ===
+        mapOfIdsAndElements.get("middle-middle-square").innerText &&
+        mapOfIdsAndElements.get("top-left-square").innerText === mapOfIdsAndElements.get("bottom-right-square").innerText &&
+        mapOfIdsAndElements.get
+        ("top-left-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("top-left-square").innerText;
+    } else if (mapOfIdsAndElements.get("top-right-square").innerText ===
+        mapOfIdsAndElements.get("middle-middle-square").innerText &&
+        mapOfIdsAndElements.get("top-right-square").innerText === mapOfIdsAndElements.get("bottom-left-square").innerText &&
+        mapOfIdsAndElements.get
+        ("top-right-square").innerText !== "") {
+        winner = mapOfIdsAndElements.get("top-right-square").innerText;
+    }
+    if (winner === "X") {
+        winnerScreen.innerText = "You win!";
+        winnerScreen.style.display = "flex";
+        xScore++;
+        xScoreElement.innerText = `X (YOU) \n ${xScore}`
+
+    } else if (winner === "O") {
+        winnerScreen.innerText = "Opponent wins!";
+        winnerScreen.style.display = "flex";
+        oScore++;
+        oScoreElement.innerText = `O (CPU) \n ${oScore}`
+    }
+}
+
 function pickRandomOpenSquare() {
     let arrayOfOpenSquares = [];
     for (let [_, value] of mapOfIdsAndElements) {
@@ -244,15 +394,22 @@ function pickRandomOpenSquare() {
     }
     let randomNumber = Math.floor(Math.random() * (arrayOfOpenSquares.length));
     arrayOfOpenSquares[randomNumber].innerText = "O";
+    arrayOfOpenSquares[randomNumber].disabled = true;
 }
 
 function determinePossibleWins() {
     let horizontalResults = rowOfTwo();
     let verticalResults = columnOfTwo();
+    let diagonalResults = diagonalOfTwo();
     if (horizontalResults[2] !== null) {
         horizontalResults[1].innerText = "O";
+        horizontalResults[1].disabled = true;
     } else if (verticalResults[2] !== null) {
         verticalResults[1].innerText = "O";
+        verticalResults[1].disabled = true;
+    } else if (diagonalResults[2] !== null) {
+        diagonalResults[1].innerText = "O";
+        diagonalResults[1].disabled = true;
     } else {
         pickRandomOpenSquare();
     }
@@ -264,19 +421,44 @@ function computerTurnToPlay() {
         pickRandomCornerSquare();
     } else if (roundCounter === 1 && mapOfIdsAndElements.get("middle-middle-square").innerText === "") {
         mapOfIdsAndElements.get("middle-middle-square").innerText = "O";
+        mapOfIdsAndElements.get("middle-middle-square").disabled = true;
     } else {
         determinePossibleWins();
     }
+    checkForWinner();
     roundCounter++;
     switchTurns();
 }
 
 for (let element of arrayOfIndividualSquareElements) {
     element.onclick = function () {
-        if (playersTurn === "X") {
-            element.innerText = "X";
+        if (!element.disabled) {
+            if (playersTurn === "X") {
+                element.innerText = "X";
+            }
+            checkForWinner();
+            if (roundCounter === 5 && winner === undefined) {
+                winnerScreen.innerText = "It's a tie!";
+                winnerScreen.style.display = "flex";
+                tiesScore++;
+                tieScoreElement.innerText = `TIES \n ${tiesScore}`
+            }
+            if (winner === undefined) {
+                switchTurns();
+                setTimeout(computerTurnToPlay, 1000);
+            }
+            element.disabled = true;
         }
-        switchTurns();
-        setTimeout(computerTurnToPlay, 1000);
     }
+}
+
+refreshButton.onclick = function () {
+    winnerScreen.style.display = "none";
+    for (let element of arrayOfIndividualSquareElements) {
+        element.disabled = false;
+        element.innerText = "";
+    }
+    playersTurn = "X";
+    roundCounter = 1;
+    winner = undefined;
 }
